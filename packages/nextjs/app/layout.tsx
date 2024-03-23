5cc10809112a03d26aa21fd9ede6778b49058489
paulo-evangelista <paulo.evangelista@sou.inteli.edu.br>
2024-03-23 04:14:22 -0300

    .

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -4,42 +4,11 @@ import { ScaffoldEthAppWithProviders } from "~~/components/ScaffoldEthAppWithPro
 import { ThemeProvider } from "~~/components/ThemeProvider";
 import "~~/styles/globals.css";
 
-const baseUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : `http://localhost:${process.env.PORT || 3000}`;
-const imageUrl = `${baseUrl}/thumbnail.jpg`;
 
 export const metadata: Metadata = {
-  metadataBase: new URL(baseUrl),
-  title: {
-    default: "Scaffold-ETH 2 App",
-    template: "%s | Scaffold-ETH 2",
-  },
-  description: "Built with 🏗 Scaffold-ETH 2",
-  openGraph: {
-    title: {
-      default: "Scaffold-ETH 2 App",
-      template: "%s | Scaffold-ETH 2",
-    },
-    description: "Built with 🏗 Scaffold-ETH 2",
-    images: [
-      {
-        url: imageUrl,
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    images: [imageUrl],
-    title: {
-      default: "Scaffold-ETH 2",
-      template: "%s | Scaffold-ETH 2",
-    },
-    description: "Built with 🏗 Scaffold-ETH 2",
-  },
-  icons: {
-    icon: [{ url: "/favicon.png", sizes: "32x32", type: "image/png" }],
-  },
+  title: "DeVolt | The descentralized charging network",
+  description: "Charge your car in the most blockchain way possible! | Built with 🏗 Scaffold-ETH 2",
+  icons: "/favicon.ico"
 };
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
