e8036a6b388ed096e8b577238736dd7ee7350f82
matheusmacedosantos <matheus.santos@sou.inteli.edu.br>
2024-03-22 21:44:05 -0300

    setup: created frontend with scaffold2

---
+++ packages/nextjs/components/Header.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import React, { useCallback, useRef, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Bars3Icon, BugAntIcon } from "@heroicons/react/24/outline";
+import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
+import { useOutsideClick } from "~~/hooks/scaffold-eth";
+
+type HeaderMenuLink = {
+  label: string;
+  href: string;
+  icon?: React.ReactNode;
+};
+
+export const menuLinks: HeaderMenuLink[] = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "Debug Contracts",
+    href: "/debug",
+    icon: <BugAntIcon className="h-4 w-4" />,
+  },
+];
+
+export const HeaderMenuLinks = () => {
+  const pathname = usePathname();
+
+  return (
+    <>
+      {menuLinks.map(({ label, href, icon }) => {
+        const isActive = pathname === href;
+        return (
+          <li key={href}>
+            <Link
+              href={href}
+              passHref
+              className={`${
+                isActive ? "bg-secondary shadow-md" : ""
+              } hover:bg-secondary hover:shadow-md focus:!bg-secondary active:!text-neutral py-1.5 px-3 text-sm rounded-full gap-2 grid grid-flow-col`}
+            >
+              {icon}
+              <span>{label}</span>
+            </Link>
+          </li>
+        );
+      })}
+    </>
+  );
+};
+
+/**
+ * Site header
+ */
+export const Header = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const burgerMenuRef = useRef<HTMLDivElement>(null);
+  useOutsideClick(
+    burgerMenuRef,
+    useCallback(() => setIsDrawerOpen(false), []),
+  );
+
+  return (
+    <div className="sticky top-0 navbar bg-base-100 min-h-0 flex-shrink-0 justify-between z-20">
+      <div className="navbar-start w-auto">
+        <Link href="/" passHref className="flex items-center gap-2 ml-4 mr-6">
+          <div className="flex relative w-[8rem] h-[4rem]">
+            <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
+          </div>
+        </Link>
+      </div>
+      <div className="navbar-end flex-grow mr-4">
+        <h1 className="ml-4 mr-4">Home</h1>
+        <h1 className="ml-4 mr-4">Buy</h1>
+        <h1 className="ml-4 mr-8">Sell</h1>
+        <RainbowKitCustomConnectButton />
+      </div>
+    </div>
+  );
+};
