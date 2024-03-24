d88d3066acdcacf8ee1f84006e44915f96c14d08
matheusmacedosantos <matheus.santos@sou.inteli.edu.br>
2024-03-24 00:49:03 -0300

    feat buttons

--- packages/nextjs/components/HomeHeroSection.tsx
+++ packages/nextjs/components/HomeHeroSection.tsx
@@ -1,24 +1,21 @@
+import Link from "next/link";
 import { Line, LineChart, ResponsiveContainer, Tooltip, XAxis } from "recharts";
 
 type ChartData = {
-    date: string;
-    value: number;
-  };
+  date: string;
+  value: number;
+};
 
-  const data: ChartData[] = [
-    { date: "15 Nov", value: 0.8 },
-    { date: "16 Nov", value: 0.2 },
-    { date: "17 Nov", value: 0.62 },
-    { date: "18 Nov", value: 0.91 },
-    { date: "19 Nov", value: 0.5 },
-  ];
-
+const data: ChartData[] = [
+  { date: "15 Nov", value: 0.8 },
+  { date: "16 Nov", value: 0.2 },
+  { date: "17 Nov", value: 0.62 },
+  { date: "18 Nov", value: 0.91 },
+  { date: "19 Nov", value: 0.5 },
+];
 
 const HomeHeroSection = () => {
-  
-    
-  
-    return (
+  return (
     <div className="grid grid-cols-1 md:grid-cols-5 md:grid-rows-2 gap-4">
       <div className="md:col-span-2 md:row-span-1 md:col-start-3 md:row-start-1">
         <div
@@ -61,12 +58,16 @@ const HomeHeroSection = () => {
         <div className="bg-green-500 text-white p-4 rounded-3xl shadow-lg flex flex-col items-center justify-center w-full h-full bg-gradient-to-tr from-black to-[#14591F]">
           <p className="text-5xl font-bold mb-8">Sell and Buy DeVolt Energy</p>
           <div className="flex space-x-4">
-            <button className="font-medium text-xl bg-[#161616] rounded-lg text-white px-8 py-3 transition  border-gray-100  border-2 hover:bg-[#37e231] hover:border-primary">
-              Buy Energy
-            </button>
-            <button className="font-medium text-xl bg-[#161616] rounded-lg text-white px-8 py-3 transition  border-gray-100  border-2 hover:bg-[#37e231] hover:border-primary">
-              Sell Energy
-            </button>
+            <Link href="/buy-energy">
+              <button className="font-medium text-xl bg-[#161616] rounded-lg text-white px-8 py-3 transition  border-gray-100  border-2 hover:bg-[#37e231] hover:border-primary">
+                Buy Energy
+              </button>
+            </Link>
+            <Link href="/buy-power">
+              <button className="font-medium text-xl bg-[#161616] rounded-lg text-white px-8 py-3 transition  border-gray-100  border-2 hover:bg-[#37e231] hover:border-primary">
+                Buy Power
+              </button>
+            </Link>
           </div>
         </div>
       </div>
@@ -88,12 +89,17 @@ const HomeHeroSection = () => {
         >
           <h1 className="h1-big p-5">Embracing Decentralized Energy Economy</h1>
 
-          <h2 className="mt-7">
+          <h2 className="mt-4">
             At DeVolt, our core mission is to dismantle the centralized paradigms of energy trade. We enable every
             household and business to take control of their energy destiny by offering a platform where they can trade
             energy assets directly. Join us to be part of a revolution that powers homes, supports communities, and
             sustains the planet with clean, peer-to-peer energy transactions.
           </h2>
+          <Link href="https://github.com/DeVolt-ETHSamba">
+            <button className="mt-3 text-s bg-[#161616] rounded-full text-white px-3 py-2 transition  hover:bg-[#2e2e2e] hover:border-primary">
+              Solution GitHub
+            </button>
+          </Link>
         </div>
       </div>
     </div>
